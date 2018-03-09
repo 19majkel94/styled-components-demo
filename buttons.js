78376bce88cb26ae8c07fa9aec5dfd3a68215f2e
Michał Lytek <michal.ly@wp.pl>
2018-03-09 20:50:57 +0100

    V4

--- buttons.js
+++ buttons.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
 
-const Button = styled.button`
+const Button = styled.button.attrs({
+  color: props => props.primary ? "#007bff" : props.danger ? "#F66" : "#888",
+})`
   background-color: transparent;
-  border: 3px solid ${props => props.primary ? "#007bff" : "#CCC"};
+  border: 3px solid ${props => props.color};
   border-radius: 10px;
   padding: 8px 16px;
   font-size: 14px;
-  color: ${props => props.primary ? "#007bff" : "#666"};
+  color: ${props => props.color};
   transition: transform .1s ease-in-out;
 
   &:hover {
-    cursor: pointer;
+    cursor: ${props => props.disabled ? "no-drop": "pointer"};
   }
   &:focus {
     outline: none;
@@ -25,5 +27,7 @@ export default () => (
   <Fragment>
     <Button>Press me!</Button>
     <Button primary>Press me!</Button>
+    <Button danger>Press me!</Button>
+    <Button primary disabled>Press me!</Button>
   </Fragment>
 );
